f46dd30b340db0a7a6d76cee8bdf0c516904a4fe
Jusley Souza <jusleysouza31@gmail.com>
2025-09-21 01:17:31 -0300

    Adding generated annotation

--- src/main/java/com/example/pix/PixExampleApplication.java
+++ src/main/java/com/example/pix/PixExampleApplication.java
@@ -3,6 +3,9 @@ package com.example.pix;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.Generated;
+
+@Generated
 @SpringBootApplication
 public class PixExampleApplication {
 
