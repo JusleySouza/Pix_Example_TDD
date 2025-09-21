930280f0b6a616a45c159ecce000865de1fa6ea7
Jusley Souza <jusleysouza31@gmail.com>
2025-09-20 21:55:04 -0300

    Class main

---
+++ src/main/java/com/example/pix/PixExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.pix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixExampleApplication.class, args);
+	}
+
+}
