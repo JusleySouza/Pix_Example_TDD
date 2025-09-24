cdbe92c84721df3400875190e91c9eac349c3825
Jusley Souza <jusleysouza31@gmail.com>
2025-09-24 01:04:46 -0300

    Refactoring PixService, setting the createdAt and status atributes, adding the mapper and changing the id to UUID

--- src/main/java/com/example/pix/service/PixService.java
+++ src/main/java/com/example/pix/service/PixService.java
@@ -1,10 +1,15 @@
 package com.example.pix.service;
 
+import com.example.pix.dto.PixRequest;
+import com.example.pix.enuns.PaymentStatus;
 import com.example.pix.exception.NotFoundException;
+import com.example.pix.mapper.PixMapper;
 import com.example.pix.model.PixPayment;
 import com.example.pix.repository.PixRepository;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
@@ -12,19 +17,28 @@ import org.springframework.stereotype.Service;
 public class PixService {
 
 	private final PixRepository repository;
+	private final PixMapper mapper;
 
-	public PixService(PixRepository repository) {
+	public PixService(PixRepository repository, PixMapper mapper) {
 		this.repository = repository;
+		this.mapper = mapper;
 	}
 
-	public PixPayment create(PixPayment pix) {
+	public PixPayment create(PixRequest pix) {
+		
 		if (pix.getAmount() == null || pix.getAmount().compareTo(BigDecimal.ZERO) <= 0) {
 			throw new IllegalArgumentException("Amount must be positive");
 		}
-		return repository.save(pix);
+		
+		PixPayment newPix = mapper.toEntity(pix);
+
+		newPix.setStatus(PaymentStatus.PROCESSING.toString());
+		newPix.setCreatedAt(LocalDateTime.now()); 
+		
+		return repository.save(newPix);
 	}
 
-	public PixPayment getById(String id) {
+	public PixPayment getById(UUID id) {
 		return repository.findById(id).orElseThrow(() -> new NotFoundException("Pix not found: " + id));
 	}
 	
