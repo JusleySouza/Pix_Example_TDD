70949a47fea4fe4e34da71fc222b33d2608273e8
Jusley Souza <jusleysouza31@gmail.com>
2025-09-25 01:24:26 -0300

    refactor(pix-service): adaptation of the create service to use the amountIsValid function

--- src/main/java/com/example/pix/service/PixService.java
+++ src/main/java/com/example/pix/service/PixService.java
@@ -1,17 +1,15 @@
 package com.example.pix.service;
 
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
 import com.example.pix.dto.PixRequest;
-import com.example.pix.enuns.PaymentStatus;
+import com.example.pix.dto.PixResponse;
 import com.example.pix.exception.NotFoundException;
 import com.example.pix.mapper.PixMapper;
 import com.example.pix.model.PixPayment;
 import com.example.pix.repository.PixRepository;
-
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.util.UUID;
-
-import org.springframework.stereotype.Service;
 
 @Service
 public class PixService {
@@ -25,21 +23,15 @@ public class PixService {
 	}
 
 	public PixPayment create(PixRequest pix) {
-		
-		if (pix.getAmount() == null || pix.getAmount().compareTo(BigDecimal.ZERO) <= 0) {
+		if (!pix.amountIsValid()) {
 			throw new IllegalArgumentException("Amount must be positive");
 		}
-		
-		PixPayment newPix = mapper.toEntity(pix);
-
-		newPix.setStatus(PaymentStatus.PROCESSING.toString());
-		newPix.setCreatedAt(LocalDateTime.now()); 
-		
-		return repository.save(newPix);
+		return repository.save(mapper.toEntity(pix));
 	}
 
-	public PixPayment getById(UUID id) {
-		return repository.findById(id).orElseThrow(() -> new NotFoundException("Pix not found: " + id));
+	public PixResponse getById(UUID id) {
+		PixPayment pix = repository.findById(id).orElseThrow(() -> new NotFoundException("Pix not found: " + id));		
+		return  mapper.toResponse(pix);
 	}
 	
 }
