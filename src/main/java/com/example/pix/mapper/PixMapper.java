fe38e7560f1d9855d58edbb5c2abc6d320e86702
Jusley Souza <jusleysouza31@gmail.com>
2025-09-25 01:12:24 -0300

    refactor(pix-mapper): adding the id, status and createdAt attributes to make the mapping

--- src/main/java/com/example/pix/mapper/PixMapper.java
+++ src/main/java/com/example/pix/mapper/PixMapper.java
@@ -1,9 +1,13 @@
 package com.example.pix.mapper;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 import org.springframework.stereotype.Component;
 
 import com.example.pix.dto.PixRequest;
 import com.example.pix.dto.PixResponse;
+import com.example.pix.enuns.PaymentStatus;
 import com.example.pix.model.PixPayment;
 
 import lombok.Generated;
@@ -19,10 +23,13 @@ public class PixMapper {
         }
         
         PixPayment payment = new PixPayment();
+        payment.setId(UUID.randomUUID());
         payment.setPayer(request.getPayer());
         payment.setReceiver(request.getReceiver());
         payment.setAmount(request.getAmount());
         payment.setDescription(request.getDescription());
+        payment.setStatus(PaymentStatus.PROCESSING.toString());
+        payment.setCreatedAt(LocalDateTime.now()); 
         return payment;
     }
 
