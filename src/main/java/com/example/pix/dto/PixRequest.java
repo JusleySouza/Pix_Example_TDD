50491727425b94a7e2211b0164b0d3e7ebc81577
Jusley Souza <jusleysouza31@gmail.com>
2025-10-19 15:24:14 -0300

    refactor(pix-request): adding validation messages

--- src/main/java/com/example/pix/dto/PixRequest.java
+++ src/main/java/com/example/pix/dto/PixRequest.java
@@ -1,8 +1,6 @@
 package com.example.pix.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Generated;
 import lombok.Getter;
@@ -23,23 +21,26 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @Schema(description = "Request para criação de um pagamento Pix")
 public class PixRequest {
 	
-    @NotBlank
+    @NotBlank(message = "{payer.not.blank}")
+    @Size(min = 3, max = 50, message = "{payer.size}")
     @Schema(description = "Identificador do pagador", type = "String", example = "payer-123")
     private String payer;
     
-    @NotBlank
+    @NotBlank(message = "{receiver.not.blank}")
+    @Size(min = 3, max = 50, message = "{receiver.size}")
     @Schema(description = "Identificador do recebedor", type = "String", example = "receiver-456")
     private String receiver;
     
-    @NotNull
-    @Positive
+    @NotNull(message = "{amount.not.null}")
+    @Positive(message = "{amount.positive}")
+    @Digits(integer = 8, fraction = 2, message = "{amount.digits}")
     @Schema(description = "Valor da transação", type = "BigDecimal", example = "150.75")
     private BigDecimal amount;
-    
+
+    @Size(max = 255, message = "{description.size}")
     @Schema(description = "Descrição da transação", type = "String", example = "Pagamento de serviços")
     private String description;
-    
-    
+
     public boolean amountIsValid() {
 		return this.amount != null && this.amount.compareTo(BigDecimal.ZERO) > 0;	
     }
