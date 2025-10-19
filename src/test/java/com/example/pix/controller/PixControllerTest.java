fb2316b4c5ecda2ddf35bf920ec6169a79109b38
Jusley Souza <jusleysouza31@gmail.com>
2025-10-18 21:59:40 -0300

    refactor(pix-controller-test): changing the library from mockito.bean to mock.bean

--- src/test/java/com/example/pix/controller/PixControllerTest.java
+++ src/test/java/com/example/pix/controller/PixControllerTest.java
@@ -16,7 +16,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.example.pix.dto.PixRequest;
@@ -36,10 +36,10 @@ class PixControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockitoBean
+    @MockBean
     private PixService service;
 
-    @MockitoBean
+    @MockBean
     private PixMapper mapper;
 
     @Test
