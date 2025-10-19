5e1fb355b8d97c81ae140f0bbb13c3f9985835ac
Jusley Souza <jusleysouza31@gmail.com>
2025-10-19 15:35:56 -0300

    refactor(pix-service): Adding the validation exception

--- src/main/java/com/example/pix/service/PixService.java
+++ src/main/java/com/example/pix/service/PixService.java
@@ -1,12 +1,16 @@
 package com.example.pix.service;
 
+import java.util.Set;
 import java.util.UUID;
 
+import com.example.pix.exception.ValidationException;
+import jakarta.validation.Validator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.pix.dto.PixRequest;
 import com.example.pix.dto.PixResponse;
-import com.example.pix.exception.NotFoundException;
+import com.example.pix.exception.ResourceNotFoundException;
 import com.example.pix.mapper.PixMapper;
 import com.example.pix.model.PixPayment;
 import com.example.pix.repository.PixRepository;
@@ -17,12 +21,16 @@ public class PixService {
 	private final PixRepository repository;
 	private final PixMapper mapper;
 
+    @Autowired
+    private Validator validator;
+
 	public PixService(PixRepository repository, PixMapper mapper) {
 		this.repository = repository;
 		this.mapper = mapper;
 	}
 
-	public PixPayment create(PixRequest pix) {
+	public PixPayment create(PixRequest pix) throws ValidationException {
+
 		if (!pix.amountIsValid()) {
 			throw new IllegalArgumentException("Amount must be positive");
 		}
@@ -30,7 +38,7 @@ public class PixService {
 	}
 
 	public PixResponse getById(UUID id) {
-		PixPayment pix = repository.findById(id).orElseThrow(() -> new NotFoundException("Pix not found: " + id));		
+		PixPayment pix = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Pix not found: " + id));
 		return  mapper.toResponse(pix);
 	}
 	
